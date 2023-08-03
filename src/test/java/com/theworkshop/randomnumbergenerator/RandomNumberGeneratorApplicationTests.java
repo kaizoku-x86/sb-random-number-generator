0739f71a9a443692ce8d541beea3eb9a06de0b88
KaizokuSwise <sdelgadofdez@gmail.com>
2023-08-03 15:43:21 +0200

    Spring Boot app init

---
+++ src/test/java/com/theworkshop/randomnumbergenerator/RandomNumberGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theworkshop.randomnumbergenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomNumberGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
