57da2308c95447f6db0ee133f3d2d1042006ce38
KaizokuSwise <sdelgadofdez@gmail.com>
2023-08-05 22:20:46 +0200

    tests added
    Signed-off-by: KaizokuSwise <sdelgadofdez@gmail.com>

--- src/main/java/com/theworkshop/randomnumbergenerator/controller/RandomNumberController.java
+++ src/main/java/com/theworkshop/randomnumbergenerator/controller/RandomNumberController.java
@@ -6,20 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 @RestController
 public class RandomNumberController {
 
     @GetMapping("/generateRandomNumbers")
-    public List<Integer> generateRandomNumbers(
+    public String generateRandomNumbers(
             @RequestParam("count") int count,
             @RequestParam("min") int min,
             @RequestParam("max") int max) {
         try{
-            List<Integer> randomNumbers = new ArrayList<>();
+            String randomNumbers = "";
 
             if (min >= max) {
                 throw new IllegalArgumentException("Invalid interval: min must be less than max");
@@ -27,7 +25,7 @@ public class RandomNumberController {
 
             Random random = new Random();
             for (int i = 0; i < count; i++) {
-                randomNumbers.add(random.nextInt(max - min + 1) + min);
+                randomNumbers=""+ (random.nextInt(max - min + 1) + min);
             }
 
             return randomNumbers;
