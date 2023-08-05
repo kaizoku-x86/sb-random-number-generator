e379fec53b0d24e3514351319d1dda7bc84e0d98
KaizokuSwise <sdelgadofdez@gmail.com>
2023-08-05 21:54:49 +0200

    tests added
    Signed-off-by: KaizokuSwise <sdelgadofdez@gmail.com>

--- src/main/java/com/theworkshop/randomnumbergenerator/controller/RandomNumberController.java
+++ src/main/java/com/theworkshop/randomnumbergenerator/controller/RandomNumberController.java
@@ -3,6 +3,7 @@ package com.theworkshop.randomnumbergenerator.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,18 +17,28 @@ public class RandomNumberController {
             @RequestParam("count") int count,
             @RequestParam("min") int min,
             @RequestParam("max") int max) {
+        try{
+            List<Integer> randomNumbers = new ArrayList<>();
 
-        List<Integer> randomNumbers = new ArrayList<>();
+            if (min >= max) {
+                throw new IllegalArgumentException("Invalid interval: min must be less than max");
+            }
+            if (!(NumberUtils.isParsable(count)&&NumberUtils.isParsable(min)&&NumberUtils.isParsable(max))) {
+                throw new NumberFormatException("Invalid interval: min must be less than max");
+            }
 
-        if (count <= 0 || min >= max) {
-            throw new IllegalArgumentException("Invalid parameters. count must be positive, and min should be less than max.");
+            Random random = new Random();
+            for (int i = 0; i < count; i++) {
+                randomNumbers.add(random.nextInt(max - min + 1) + min);
+            }
+
+            return randomNumbers;
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
         }
-
-        Random random = new Random();
-        for (int i = 0; i < count; i++) {
-            randomNumbers.add(random.nextInt(max - min + 1) + min);
-        }
-
-        return randomNumbers;
     }
 }
