9b29ae43c19f49ef6e306bc554c2690a0e23c628
KaizokuSwise <sdelgadofdez@gmail.com>
2023-08-05 22:10:23 +0200

    tests added
    Signed-off-by: KaizokuSwise <sdelgadofdez@gmail.com>

--- src/main/java/com/theworkshop/randomnumbergenerator/controller/RandomNumberController.java
+++ src/main/java/com/theworkshop/randomnumbergenerator/controller/RandomNumberController.java
@@ -1,6 +1,6 @@
 package com.theworkshop.randomnumbergenerator.controller;
 
-import org.springframework.http.httpStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
